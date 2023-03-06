dcd6586868713df04fcec583b2b2fedc5f56afce
ritikk786 <kumarritik9540@gmail.com>
2023-03-06 12:57:40 +0530

    advance react task2

--- src/componenets/Expenses/ExpenseDetail.js
+++ src/componenets/Expenses/ExpenseDetail.js
@@ -1,12 +1,22 @@
 import './ExpenseItem.css'
 
 const ExpenseDetail= (props)=>{
+
+
+    const clickhandler = ()=>{
+        console.log('clicked')
+    }
+    const deletehandler =()=>{
+
+    }
 return(
 <div className='expense-item__description'>
 
                 <h2  >{props.detail.title}</h2>
                 <h2  >{props.place}</h2>
                 <div className='expense-item__price'>${props.detail.amount}</div>
+                <button onClick={clickhandler}>Click on me</button>
+                <button onClick={deletehandler}>Delete</button>
             </div>
 )
 }
