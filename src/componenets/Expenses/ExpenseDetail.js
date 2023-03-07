689f75aad6a87a2a6e0131dbfd9af0392ebbbcca
ritikk786 <kumarritik9540@gmail.com>
2023-03-07 21:26:53 +0530

    task3

--- src/componenets/Expenses/ExpenseDetail.js
+++ src/componenets/Expenses/ExpenseDetail.js
@@ -1,24 +1,37 @@
+
+import { useState } from 'react'
 import './ExpenseItem.css'
 
 const ExpenseDetail= (props)=>{
 
+    const [title, setTitle] = useState(props.detail.title)
+
+    const [amount, setAmount] = useState(props.detail.amount)
 
     const clickhandler = ()=>{
-        console.log('clicked')
+        setTitle('updated')
+
     }
 
     const deletehandler = (e)=>{
-        e.target.parentNode.parentNode.remove()
+       e.target.parentNode.parentNode.remove()
     }
+
+    const amounthandler = () =>{
+        setAmount(100)
+        console.log(100)
+    }
+
 
 return(
 <div className='expense-item__description'>
 
-                <h2  >{props.detail.title}</h2>
+                <h2  >{title}</h2>
                 <h2  >{props.place}</h2>
-                <div className='expense-item__price'>${props.detail.amount}</div>
+                <div className='expense-item__price'>${amount}</div>
                 <button onClick={clickhandler}>Click on me</button>
-                <button onClick={deletehandler}>Delete</button>
+                <button onClick={deletehandler} >Delete</button>
+                <button onClick={amounthandler} >100</button>
             </div>
 )
 }
