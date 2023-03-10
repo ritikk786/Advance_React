d59dd28f379dc8b3ae0931c83bcaa901875e1388
ritikk786 <kumarritik9540@gmail.com>
2023-03-10 10:56:21 +0530

    task 6

--- src/componenets/NewExpenses/ExpenseForm.js
+++ src/componenets/NewExpenses/ExpenseForm.js
@@ -1,11 +1,34 @@
+import { useState } from 'react'
+
 import './ExpenseForm.css'
 
 const ExpenseForm =()=>{
+    const [enteredtitle , setenteredtitle ] = useState('')
+    const [enteredAmount , setenteredAmount ] = useState('')
+    const [enteredDate , setenteredDate ] = useState('')
+
+    const amountchangehandler=(event)=>{
+        setenteredAmount(event.target.value)
+    }
     const titlechangehandler=(event)=>{
-        console.log(event.target.value)
+        setenteredtitle (event.target.value)
+    }
+
+    const datechangehandler=(event)=>{
+        setenteredDate(event.target.value)
+    }
+
+    const printdetailhandler=(e)=>{
+        e.preventDefault();
+        const expenseData = {
+            title :enteredtitle,
+            amount:enteredAmount,
+            date: new Date(enteredDate)
+        }
+        console.log(expenseData)
     }
     return (
-        <form>
+        <form onSubmit={printdetailhandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
@@ -13,14 +36,14 @@ const ExpenseForm =()=>{
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number'/>
+                    <input type='number' onChange={amountchangehandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2019-01-01' max='2022-12-31'/>
+                    <input type='date' min='2019-01-01' max='2022-12-31' onChange={datechangehandler}/>
                 </div>
                 <div className='new-expense__actions'>
-                    <button type='submit'>Delete</button>
+                    <button type='submit'>Add</button>
                 </div>
             </div>
         </form>
